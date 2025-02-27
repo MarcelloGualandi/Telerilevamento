706d0964964a7f9e316fef70ca51d9d3abc1d14a
MarcelloGualandi <marcellogualandi96@gmail.com>
2025-02-27 15:54:47 +0100

    Update first_script.R

--- code/first_script.R
+++ code/first_script.R
@@ -3,3 +3,27 @@
 
 anna <- 2 + 3 # assign an operation to an object
 chiara <- 4 + 6
+
+filippo <-  c(0.2, 0.4, 0.6, 0.8, 0.9)  # array 
+filippo
+
+luca <- c(100, 80, 60, 50, 10)
+luca
+
+plot(luca,filippo)
+plot(luca,filippo,pch=19)
+plot(luca,filippo,pch=19, col="blue")
+
+plot(luca,filippo,pch=19, col="blue",cex=2)
+
+plot(luca,filippo,pch=19, col="blue",cex=2, xlab="rubbish", ylab="biomass")
+
+# Installing packages
+# CRAN
+install.packages("terra")
+library(terra)
+
+install.packages("devtools")
+library(devtools)
+
+install_github("ducciorocchini/imageRy")
