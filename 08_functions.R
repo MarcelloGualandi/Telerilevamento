1e1fb2bc27b32315cadd7dd346bfb25ca206540b
MarcelloGualandi <marcellogualandi96@gmail.com>
2026-04-23 15:43:44 +0200

    Create 08_functions.R

---
+++ 08_functions.R
@@ -0,0 +1,23 @@
+# My functions!
+definiamo gli argoment della funzione
+
+somma <- function(x,y){
+  z=x+y
+  return(z)
+  }
+
+
+> somma(1,100)
+[1] 101
+
+# Exercise: make the function called difference
+
+difference <- function(x,y){
+  z=x-y
+  return(z)
+  }
+
+> difference(300,150)
+[1] 150
+
+
