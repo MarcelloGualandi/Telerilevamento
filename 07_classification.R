3c8ebdc6e943b3c09b42113d96c2f72a2d6b2a2f
MarcelloGualandi <marcellogualandi96@gmail.com>
2026-04-23 14:51:28 +0200

    Update 07_classification.R

--- 07_classification.R
+++ 07_classification.R
@@ -4,6 +4,8 @@
 
 library(terra)
 library(imageRy)
+library(ggplot2)
+library(patchwork)
 
 #listing files
 im.list
@@ -105,8 +107,77 @@ perc2006<-prop2006*100
 
 # Table
 tabout<-data.frame(
-  class=c("Forest","Human"
+  class=c("Forest","Human"),
           perc1992=c(83, 17),
           perc2006=c(45, 55)
           )
-  
+
+ggplot(tabout, aes(x=class,y=perc1992, color=class)) + # structure
+  geom_bar(stat="identity", fill="white") # barplot. posso usare questi commenti per spiegare cosa ho fatto
+
+##all'interno dobbiamo mettere che cosa vogliamo rendere un grafico e come realizzarlo. dentro le estetiche aes, ci sono tre parametri: x, y e colore. 
+dobbiamo dire a ggplot che tipo di grafico vogliamo fare. noi ora vogliamo creare un istogramma. 
+
+facciamo la stessa cosa per il 2006
+## exercise: plot the bars of 2006
+
+ggplot(tabout, aes(x=class,y=perc2006, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")
+
+c'è un pacchetto che si chiama patchwork che dialoga con ggplot. non fa altro che associare pezzi di ggplot a degli oggetti.
+poi posso sommare i diversi oggetti e confrontarli.
+
+ora associamo i nostri 2 grafici a 2 oggetti, p1 e p2 per esempio
+
+# Using patchworl
+p1<-ggplot(tabout, aes(x=class,y=perc1992, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")
+p2<-ggplot(tabout, aes(x=class,y=perc2006, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")
+
+p1 + p2
+
+## mettiamo le 2 y uguale, perchè se vediamo i 2 grafici che ho appena creato le y sono diverse. queste cose le fanno i politici per fregarvi
+p1<-ggplot(tabout, aes(x=class,y=perc1992, color=class)) + # structure
+  geom_bar(stat="identity", fill="white") + # bar plot
+ylim(c(0,100)) # limits
+
+p2<-ggplot(tabout, aes(x=class,y=perc2006, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")  + # bar plot
+ylim(c(0,100)) # limits
+
+> p1 + p2
+
+Rimuoviamo la legende, in questo caso in solo uno dei due grafici ma lo puoi fare anche nell'altro
+p1<-ggplot(tabout, aes(x=class,y=perc1992, color=class)) + # structure
+  geom_bar(stat="identity", fill="white") + # bar plot
+ylim(c(0,100)) # limits
+theme(legend.position="none")   # removing legend
+
+p2<-ggplot(tabout, aes(x=class,y=perc2006, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")  + # bar plot
+ylim(c(0,100)) # limits
+
+
+
+p1<-ggplot(tabout, aes(x=class,y=perc1992, color=class)) + # structure
+  geom_bar(stat="identity", fill="white") + # bar plot
+ylim(c(0,100)) # limits
+theme(legend.position="none")   # removing legend
+theme_minimal() ## per cambiare lo sfondo
+
+p2<-ggplot(tabout, aes(x=class,y=perc2006, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")  + # bar plot
+ylim(c(0,100)) # limitsggplot(tabout, aes(x=class, y=perc1992, color=class)) + 
+      geom_bar(stat="identity", fill="white") 
+
+p1<-ggplot(tabout, aes(x=class,y=perc1992, color=class)) + # structure
+  geom_bar(stat="identity", fill="white") + # bar plot
+ylim(c(0,100)) # limits
+theme(legend.position="none")   # removing legend
+theme_dark()  #cambia lo sfondo
+
+p2<-ggplot(tabout, aes(x=class,y=perc2006, color=class)) + # structure
+  geom_bar(stat="identity", fill="white")  + # bar plot
+ylim(c(0,100)) # limitsggplot(tabout, aes(x=class, y=perc1992, color=class)) + 
+      geom_bar(stat="identity", fill="white") 
