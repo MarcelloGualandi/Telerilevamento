5bd3327b224fed2cdb3a7edfe9280eeaf7a64cb7
MarcelloGualandi <marcellogualandi96@gmail.com>
2026-07-18 16:49:25 +0200

    Update code.R

--- Esame/code.R
+++ Esame/code.R
@@ -25,14 +25,18 @@ plotRGB(bk2025, r="B4", g="B3", b="B2", stretch="hist",
 range(bk2007)
 range(bk2025)
 
-norm <- function(x) (x - min(x[], na.rm=TRUE)) / (max(x[], na.rm=TRUE) - min(x[], na.rm=TRUE))
+norm <- function(x) {
+  (x - min(x[], na.rm = TRUE)) /
+    (max(x[], na.rm = TRUE) - min(x[], na.rm = TRUE))
+}
+
 
 # Visualizzazione separata delle quattro bande (RGB e NIR) per entrambe le immagini    
 
 im.multiframe(2,2)
-plot(norm(bk2007[[3]]), main="B4 - Red", col = magma(100))
+plot(norm(bk2007[[1]]), main="B4 - Red", col = magma(100))
 plot(norm(bk2007[[2]]), main="B3 - Green", col = magma(100))
-plot(norm(bk2007[[1]]), main="B2 - Blue", col = magma(100))
+plot(norm(bk2007[[3]]), main="B2 - Blue", col = magma(100))
 plot(norm(bk2007[[4]]), main="B8 - NIR", col = magma(100))
 
 
@@ -47,8 +51,8 @@ plot(bk2025[[4]], main = "B8 - NIR", col = magma(100))
 norm <- function(x) (x - min(x[], na.rm=TRUE)) / (max(x[], na.rm=TRUE) - min(x[], na.rm=TRUE))
 
 im.multiframe(1,2)
-plot(norm(bk2007[[5]]),main="B12 - SWIR2", col = magma(100))
-plot(norm(bk2025[[5]]),main="B12 - SWIR2", col = magma(100))
+plot(norm(bk2007[[5]]),main="B12 - SWIR2, 2007", col = magma(100))
+plot(norm(bk2025[[5]]),main="B12 - SWIR2, 2025", col = magma(100))
 
 ## Sostituendo il NIR al posto della banda del red, si evidenziano le zone di vegetazione (blu) e tutto ciò che non è vegetazione (giallo).
 im.multiframe(1,2)
@@ -67,14 +71,12 @@ plot(dvi_2025, col = viridis(100), main = "DVI 2025")
 
 # Calcolo e visualizzazione differenza DVI  
 ## i due raster NON hanno la stessa estensione, dimensione o risoluzione QUINDI dobbiamo portare il 2007 alla stessa risoluzione del 2025
-
-bk2007_norm_res <- terra::resample(bk2007_norm, bk2025, method="bilinear")
-dvi_2007_res <- bk2007_norm_res[["B4"]] - bk2007_norm_res[["B3"]]
-dvi_2025_res <- bk2025[["B4"]] - bk2025[["B3"]]
-dvi_diff <- dvi_2025_res - dvi_2007_res
+bk2007_res <- terra::resample(bk2007, bk2025, method = "bilinear")
+dvi_2007 <- bk2007_res[["B8"]] - bk2007_res[["B4"]]
+dvi_2025 <- bk2025[["B8"]] - bk2025[["B4"]]
+dvi_diff <- dvi_2025 - dvi_2007
 im.multiframe(1,1)
 plot(dvi_diff, col = magma(100), main = "Differenza DVI (2025 - 2007)")
-
 
 
 # Analisi NDVI
@@ -182,96 +184,6 @@ p2 <- ggplot(tab, aes(x = classi, y = a2025, fill = classi)) +
 p1 + p2      # Grazie al pacchetto patchwork si possono unire i grafici in questo modo
 
 
-## utilizziamo altre soglie per definire più classi. 5 classi
-
-class_matrix_sahel <- matrix(c(
-  -Inf, 0.05, 0,   # Ombra, roccia, acqua, suolo molto scuro
-  0.05, 0.20, 1,  # Suolo nudo / vegetazione erbacea molto rada
-  0.20, 0.35, 2,  # Vegetazione erbacea/arbustiva discontinua
-  0.35, 0.50, 3,  # Vegetazione arbustiva/arborea moderata (GGW in crescita)
-  0.50, Inf, 4    # Vegetazione arborea/arbustiva densa (nuclei di successo GGW)
-), 
-ncol = 3, byrow = TRUE)
-
-class_matrix_sahel
-#     [,1] [,2] [,3]
-# [1,] -Inf 0.05    0
-# [2,] 0.05 0.20    1
-# [3,] 0.20 0.35   2
-# [4,] 0.35 0.50    3
-# [5,] 0.50  Inf    4
-
-ndvi_2007_cl <- classify(ndvi_2007, class_matrix_sahel)
-ndvi_2025_cl <- classify(ndvi_2025, class_matrix_sahel)
-pal_sahel_cb <- c(
-  "#000000",  # Classe 0 - ombra / roccia / acqua
-  "#E41A1C",  # Classe 1 - suolo nudo
-  "#377EB8",  # Classe 2 - erbacee / arbusti bassi
-  "#4DAF4A",  # Classe 3 - arbusti attivi
-  "#984EA3"   # Classe 4 - vegetazione densa
-)
-
-im.multiframe(1, 2)
-plot(ndvi_2007_cl, col = pal_sahel_cb, main = "NDVI class. 2007")
-plot(ndvi_2025_cl, col = pal_sahel_cb, main = "NDVI class. 2025")
-
-# Calcolo percentuali
-# Frequenze
-freq_2007 <- freq(ndvi_2007_cl)
-freq_2025 <- freq(ndvi_2025_cl)
-
-# Percentuali
-# trasformiamo in percentuali garantendo tre classi
-
-tot2007 <- sum(freq_2007$count)
-tot2025 <- sum(freq_2025$count)
-
-perc_2007 <- c(
-  ifelse(1 %in% freq_2007$value, freq_2007$count[freq_2007$value==1] / tot2007, 0),
-  ifelse(2 %in% freq_2007$value, freq_2007$count[freq_2007$value==2] / tot2007, 0),
-  ifelse(3 %in% freq_2007$value, freq_2007$count[freq_2007$value==3] / tot2007, 0)
-)
-
-perc_2025 <- c(
-  ifelse(1 %in% freq_2025$value, freq_2025$count[freq_2025$value==1] / tot2025, 0),
-  ifelse(2 %in% freq_2025$value, freq_2025$count[freq_2025$value==2] / tot2025, 0),
-  ifelse(3 %in% freq_2025$value, freq_2025$count[freq_2025$value==3] / tot2025, 0)
-)
-tab <- data.frame(
-  Classe = c("0 - Ombra/suolo scuro",
-             "1 - Suolo nudo",
-             "2 - Erbacee/arbusti bassi",
-             "3 - Arbusti attivi",
-             "4 - Vegetazione densa"),
-  Perc_2007 = round(perc_2007, 2),
-  Perc_2025 = round(perc_2025, 2)
-)
-
-print(tab)
-
-
-# Visualizzazione
-tab <- data.frame(
-  classi = c("Suolo nudo", "Vegetazione media", "Vegetazione sana"),
-  a2007 = round(perc_2007 * 100, 2),
-  a2025 = round(perc_2025 * 100, 2)
-)
-print(tab)
-p1 <- ggplot(tab, aes(x = classi, y = a2007, fill = classi)) +    
-  geom_bar(stat = "identity") +
-  scale_fill_viridis_d() +
-  ylim(0, 100) +
-  labs(title = "Classi NDVI 2007", y = "%", x = NULL) +
-  theme_minimal()
-
-p2 <- ggplot(tab, aes(x = classi, y = a2025, fill = classi)) +
-  geom_bar(stat = "identity") +
-  scale_fill_viridis_d() +
-  ylim(0, 100) +
-  labs(title = "Classi NDVI 2025", y = "%", x = NULL) +
-  theme_minimal()
-
-p1 + p2      # Grazie al pacchetto patchwork si possono unire i grafici in questo modo
 
 ## Ora facciamolo con le 5 classi
 
@@ -288,7 +200,8 @@ class_matrix_sahel
 ndvi_2007_cl <- classify(ndvi_2007, class_matrix_sahel)
 ndvi_2025_cl <- classify(ndvi_2025, class_matrix_sahel)
 
-im.multiframe (1,2)
+
+im.multiframe(1, 2)
 plot(ndvi_2007_cl, col = viridis(5), main = "NDVI class. 2007")
 plot(ndvi_2025_cl, col = viridis(5), main = "NDVI class. 2025")
 
@@ -359,9 +272,9 @@ p1 + p2
 ## Analisi multitemporale
 # L'analisi multitemporale ha permesso di confrontare i dati telerilevati del 2007 e del 2025, focalizzandosi in particolare sulla banda del NIR e sull'indice NDVI, al fine di evidenziare variazioni significative nello stato della vegetazione nell’arco di quasi vent'anni.
 # rinominiamo le bande e portiamo il NIR del 2007 alla griglia del 2025
-nir_2007 <- bk2007[["B4"]] / 10   # riscalato
-nir_2007_res <- terra::resample(nir_2007, bk2025[[4]], method="bilinear")
-nir_diff <- bk2025[[4]] - nir_2007_res
+bk2007_res <- terra::resample(bk2007, bk2025, method = "bilinear")
+nir_2007_res <- terra::resample(nir_2007, bk2025[["4"]], method="bilinear")
+nir_diff <- bk2025[["4"]] - nir_2007_res
 
 ## stessa cosa per il NDVI
 ndvi_2007_res <- terra::resample(ndvi_2007, ndvi_2025, method="bilinear")
